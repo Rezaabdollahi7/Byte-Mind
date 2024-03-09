923c9ccd398c27ffd1ae1cd9990d40abdffe6279
Rezaabdollahi7 <srezaabdollahi7@gmail.com>
2024-03-09 23:48:47 +0330

    Fix ( Company Logo ) : { add mobile & Tablet Responsive Design }

--- src/components/CompanyLogos.jsx
+++ src/components/CompanyLogos.jsx
@@ -6,12 +6,9 @@ const CompanyLogos = ({ className }) => {
       <h5 className="tagline mb-6 text-center text-n-1/50">
         Helping people create beautiful content at
       </h5>
-      <ul className="flex">
+      <ul className="grid grid-cols-1 md:grid-cols-2 md:gap-20 md:gap-y-0  xl:grid-cols-4 mt-16 mx-auto w-max xl:gap-40">
         {companyLogos.map((logo, index) => (
-          <li
-            className="flex items-center justify-center flex-1 h-[8.5rem]"
-            key={index}
-          >
+          <li className="col-span-1 h-[8.5rem]" key={index}>
             <img src={logo} width={134} height={28} alt={logo} />
           </li>
         ))}
